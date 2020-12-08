61852df4f7ab6f04c74d751cf16b0a2f0b82074a
IgorTeslenko <igor.teslenko@gmail.com>
2020-12-08 13:54:36 +0100

    Homework 3 fix

--- src/main/java/homework3/InvertString.java
+++ src/main/java/homework3/InvertString.java
@@ -1,11 +1,18 @@
 package homework3;
 
+import homework3.utils.MyStack;
+
 public class InvertString {
 
     public static String invertString(String original) {
         StringBuilder sb = new StringBuilder();
-        for (int i = original.toCharArray().length - 1; i >= 0; i--) {
-            sb.append(original.toCharArray()[i]);
+        MyStack<Character> strStack = new MyStack<>();
+        for (char c : original.toCharArray()) {
+            strStack.push(c);
+        }
+        int j = strStack.size();
+        for (int i = 0; i < j; i++) {
+            sb.append(strStack.pop());
         }
         return sb.toString();
     }
